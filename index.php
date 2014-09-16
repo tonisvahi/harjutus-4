c18bd084dac746eb0966999dc794480ee0e27802
Tõnis Vähi <t6nis.v2hi@gmail.com>
2014-09-16 09:55:56 +0300

    Andmetüübi funktsioonid

--- index.php
+++ index.php
@@ -15,6 +15,14 @@
       			<?php echo round($float, 1); ?><br>
       			<?php echo ceil($float); ?><br>
 			 <?php echo floor($float); ?>
+		<h2>Andmetüübi funktsioonid</h2>
+      			<?php $integer = 3; ?><br>
+      			<?php echo "Kas {$integer} on integer? " . is_int($integer); ?><br>
+      			<?php echo "Kas {$float} on integer? " . is_int($float); ?><br>
+      			<?php echo "Kas {$integer} on float? " . is_float($integer); ?><br>
+      			<?php echo "Kas {$float} on float? " . is_float($float); ?><br>
+      			<?php echo "Kas {$integer} on number? " . is_numeric($integer); ?><br>
+      			<?php echo "Kas {$float} on number? " . is_numeric($float); ?>
    </body>
 
 </html>
