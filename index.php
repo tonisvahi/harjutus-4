c47b3141c8aa0e64f08f1130d5ad67dd6091fc09
tonisvahi <t6nis.v2hi@gmail.com>
2014-09-16 08:54:01 +0300

    Added index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+   <head>
+   <title>Harjutus4</title>
+	 <meta http-equiv="Content-Type" content="text/html;
+   cahrset=utf-8">
+   <title>Andmetüübid (2)</title>
+	 </head>
+   <body>
+
+   </body>
+
+</html>
