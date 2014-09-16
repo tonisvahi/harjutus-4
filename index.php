15f4762a1cef830833365c3fb84c3c251e9637ef
Tõnis Vähi <t6nis.v2hi@gmail.com>
2014-09-16 09:44:14 +0300

    Murdarvude ümardamine
    Mistake correction: cahrset --> charset

--- index.php
+++ index.php
@@ -3,14 +3,18 @@
    <head>
    <title>Harjutus4</title>
 	 <meta http-equiv="Content-Type" content="text/html;
-   cahrset=utf-8">
+   charset=utf-8">
    <title>Andmetüübid (2)</title>
 	 </head>
    <body>
 		<h2>Arvutamine murd- ja täisarvudega</h2>
-		<?php echo $float = 3.14; ?><br>
-		<?php echo $float + 7; ?><br>
-		<?php echo 4/3; ?><br>
+			<?php echo $float = 3.14; ?><br>
+			<?php echo $float + 7; ?><br>
+			<?php echo 4/3; ?><br>
+		<h2>Murdarvude ümardamine</h2>
+      			<?php echo round($float, 1); ?><br>
+      			<?php echo ceil($float); ?><br>
+			 <?php echo floor($float); ?>
    </body>
 
 </html>
