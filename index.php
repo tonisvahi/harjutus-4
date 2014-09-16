6ebf0a0e5186fdd6bd39ab1b3365ff08fef5c30f
Tõnis Vähi <t6nis.v2hi@gmail.com>
2014-09-16 09:09:35 +0300

    Arvutamine murd- ja täisarvudega

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
    <title>Andmetüübid (2)</title>
 	 </head>
    <body>
-
+		<h2>Arvutamine murd- ja täisarvudega</h2>
+		<?php echo $float = 3.14; ?><br>
+		<?php echo $float + 7; ?><br>
+		<?php echo 4/3; ?><br>
    </body>
 
 </html>
